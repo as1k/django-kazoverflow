cffea8e895ac2b0162b34ab763ed09b0a8b403cd
Assylanbek Nurmukhambet <nurmukhambet.ak@gmail.com>
2020-04-23 00:51:07 +0600

    cbv 2nd part

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
 
-from api.views.cbv import CategoryList, CategoryDetails, DiscussionList, DiscussionDetails, CategoryDiscussions
+from api.views.cbv import CategoryList, CategoryDetails, DiscussionList, DiscussionDetails, CategoryDiscussions, \
+    TopicList, TopicDetails, DiscussionTopics
 
 urlpatterns = [
     path('categories/', CategoryList.as_view()),
     path('categories/<int:pk>/', CategoryDetails.as_view()),
     path('discussions/', DiscussionList.as_view()),
     path('discussions/<int:pk>/', DiscussionDetails.as_view()),
-    path('categories/<int:category_id>/discussions/', CategoryDiscussions.as_view())
+    path('categories/<int:category_id>/discussions/', CategoryDiscussions.as_view()),
+    path('topics/', TopicList.as_view()),
+    path('topics/<int:pk>/', TopicDetails.as_view()),
+    path('categories/<int:category_id>/discussions/<int:discussion_id>/topics/', DiscussionTopics.as_view()),
 ]
--- api/views/cbv.py
+++ api/views/cbv.py
@@ -2,11 +2,17 @@ from rest_framework import status, generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from api.models import Category, Discussion
-from api.serializers import CategorySerializer, DiscussionSerializer
+from api.models import Category, Discussion, Topic, Comment
+from api.serializers import CategorySerializer, DiscussionSerializer, TopicSerializer, CommentSerializer
 
 
 class CategoryList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    # permission_classes = (IsAuthenticated,)
+
+
+class CategoryDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     # permission_classes = (IsAuthenticated,)
@@ -18,12 +24,6 @@ class DiscussionList(generics.ListCreateAPIView):
     # permission_classes = (IsAuthenticated,)
 
 
-class CategoryDetails(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-    # permission_classes = (IsAuthenticated,)
-
-
 class DiscussionDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Discussion.objects.all()
     serializer_class = DiscussionSerializer
@@ -32,12 +32,67 @@ class DiscussionDetails(generics.RetrieveUpdateDestroyAPIView):
 
 class CategoryDiscussions(APIView):
     def get(self, request, category_id):
-        vacancies = Discussion.objects.filter(category_id=category_id)
-        serializer = DiscussionSerializer(vacancies, many=True)
+        discussions = Discussion.objects.filter(category_id=category_id)
+        serializer = DiscussionSerializer(discussions, many=True)
         return Response(serializer.data)
 
     def post(self, request):
         serializer = DiscussionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': serializer.errors},status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class TopicList(generics.ListCreateAPIView):
+    queryset = Discussion.objects.all()
+    serializer_class = DiscussionSerializer
+    # permission_classes = (IsAuthenticated,)
+
+
+class TopicDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Discussion.objects.all()
+    serializer_class = DiscussionSerializer
+    # permission_classes = (IsAuthenticated,)
+
+
+class DiscussionTopics(APIView):
+    def get(self, request, category_id, discussion_id):
+        discussions = Discussion.objects.filter(category_id=category_id)
+        topics = Topic.objects.filter(discussion_id=discussion_id)
+        serializer = TopicSerializer(topics, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TopicSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': serializer.errors},status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Discussion.objects.all()
+    serializer_class = DiscussionSerializer
+    # permission_classes = (IsAuthenticated,)
+
+
+class CommentDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Discussion.objects.all()
+    serializer_class = DiscussionSerializer
+    # permission_classes = (IsAuthenticated,)
+
+
+class TopicComments(APIView):
+    def get(self, request, category_id, discussion_id, topic_id):
+        discussions = Discussion.objects.filter(category_id=category_id)
+        topics = Topic.objects.filter(discussion_id=discussion_id)
+        comments = Comment.objects.filter(topic_id=topic_id)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TopicSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
